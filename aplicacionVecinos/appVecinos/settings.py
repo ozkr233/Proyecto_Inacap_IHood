1d2d96ae5c274c382f4132eb23977056e06e472a
Oscar Alef Ibarra Ortega <alefito2015@hotmail.com>
2024-11-26 02:03:28 -0300

    Update settings.py

--- aplicacionVecinos/appVecinos/settings.py
+++ aplicacionVecinos/appVecinos/settings.py
@@ -124,7 +124,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-AUTH_USER_MODEL = 'api.Usuario'
+# AUTH_USER_MODEL = 'api.Usuario'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
