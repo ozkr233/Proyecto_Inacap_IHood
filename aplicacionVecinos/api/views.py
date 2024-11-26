7b87a940827e1135d6a8b66fcea174d456693c7d
Oscar Alef Ibarra Ortega <alefito2015@hotmail.com>
2024-11-25 22:26:50 -0300

    Add files via upload

--- aplicacionVecinos/api/views.py
+++ aplicacionVecinos/api/views.py
@@ -9,6 +9,7 @@ from .serializers import (
     EvidenciaSerializer,
     UsuarioRegistroSerializer
 )
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 # ViewSet para Publicacion
@@ -20,10 +21,19 @@ class RegistroUsuarioViewSet(viewsets.ViewSet):
     def create(self, request):
         serializer = UsuarioRegistroSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()  # Crea el nuevo usuario
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            user = serializer.save()  # Crea el nuevo usuario
+            # Generar tokens JWT
+            refresh = RefreshToken.for_user(user)
+            access = refresh.access_token
+
+            respose_data = {
+                'user': UsuarioRegistroSerializer(user).data,  # Incluye datos del usuario
+                'refresh': str(refresh),
+                'access': str(access),
+            }
+
+            return Response(respose_data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
 
 # ViewSet para Categoria
 class CategoriaViewSet(viewsets.ModelViewSet):
